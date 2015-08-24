27078743ee05dbe564754ca20e88e069b9b84239
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-08-24 20:31:33 +0200

    more

--- forcing.v
+++ forcing.v
@@ -22,7 +22,14 @@ Inductive term :=
 | fvar : Var.t -> term
 | bvar : nat -> term
 | appl : term -> term -> term
-| abst : term -> term.
+| abst : term -> term
+| comp : term -> term -> term.
+
+Bind Scope trm_scope with term.
+Delimit Scope trm_scope with term.
+
+Notation "'λ' t" := (abst t) (at level 80, t at level 0, format "'λ'  t") : trm_scope.
+Notation "t @ u" := (appl t u) (at level 20, left associativity) : trm_scope.
 
 Fixpoint open (t : term) (n : nat) (r : term) :=
 match t with
@@ -30,6 +37,7 @@ match t with
 | bvar m => if PeanoNat.Nat.eq_dec m n then r else bvar m
 | appl t u => appl (open t n r) (open u n r)
 | abst t => abst (open t (S n) r)
+| comp t u => comp (open t n r) (open u n r)
 end.
 
 Notation "t << u" := (open t 0 u) (at level 50, left associativity).
@@ -40,6 +48,7 @@ Inductive Term : term -> Prop :=
 | Term_abst : forall L t,
   (forall x, ~ VSet.In x L -> Term (t << fvar x)) ->
   Term (abst t)
+| Term_comp : forall t u, Term t -> Term u -> Term (comp t u)
 .
 
 Fixpoint subst (t : term) (x : Var.t) (r : term) :=
@@ -48,6 +57,7 @@ match t with
 | bvar m => bvar m
 | appl t u => appl (subst t x r) (subst u x r)
 | abst t => abst (subst t x r)
+| comp t u => comp (subst t x r) (subst u x r)
 end.
 
 Notation "[ t | x := r ]" := (subst t x r).
@@ -57,6 +67,21 @@ Inductive red : term -> term -> Prop :=
 | red_appl_l : forall t u r, red t r -> red (appl t u) (appl r u)
 | red_appl_r : forall t u r, red u r -> red (appl t u) (appl t r).
 
+Definition lift1 x β t := subst t x (λ λ (fvar x @ bvar 1 @ t)).
+
+Fixpoint lift σ β t {struct t} :=
+match t with
+| fvar x => if VSet.mem x σ then else
+| bvar m => bvar m
+| appl t u => appl (subst t x r) (subst u x r)
+| abst t => abst (subst t x r)
+end.
 
 
+Fixpoint forcing σ ω t {struct t} :=
+match t with
+| bvar n => bvar n
+| fvar x => if VSet.mem x σ then t else t
+| _ => fvar ω
+end.
 
