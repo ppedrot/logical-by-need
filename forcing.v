7f75b13fd0834d70daadfcbb56f0ac3c0ea3eacb
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-08-24 20:22:14 +0200

    more

--- forcing.v
+++ forcing.v
@@ -1,9 +1,9 @@
-Require FSets FMaps NArith Wellfounded.
+Require MSets NArith Wellfounded.
 Require Import Program Setoid Morphisms BinNat Relations.
 
 Module Type Fresh
-  (Var : OrderedType.OrderedType)
-  (VSet : FSetInterface.Sfun(Var)).
+  (Var : Orders.UsualOrderedType)
+  (VSet : MSetInterface.SetsOn(Var)).
 
 Parameter fresh : forall s : VSet.t, {v | ~ VSet.In v s}.
 
@@ -11,12 +11,12 @@ End Fresh.
 
 
 Module Spec
-  (Var : OrderedType.OrderedType)
-  (VSet : FSetInterface.Sfun(Var))
+  (Var : Orders.UsualOrderedType)
+  (VSet : MSetInterface.SetsOn(Var))
   (Import Fresh : Fresh(Var)(VSet))
 .
 
-Module Import USetFacts := FSetFacts.WFacts_fun(Var)(VSet).
+Module Import VSetFacts := MSetFacts.WFactsOn(Var)(VSet).
 
 Inductive term :=
 | fvar : Var.t -> term
@@ -27,21 +27,35 @@ Inductive term :=
 Fixpoint open (t : term) (n : nat) (r : term) :=
 match t with
 | fvar x => fvar x
-| bvar m => if Nat.eqb m n then r else bvar m
+| bvar m => if PeanoNat.Nat.eq_dec m n then r else bvar m
 | appl t u => appl (open t n r) (open u n r)
 | abst t => abst (open t (S n) r)
 end.
+
+Notation "t << u" := (open t 0 u) (at level 50, left associativity).
 
 Inductive Term : term -> Prop :=
 | Term_fvar : forall x, Term (fvar x)
 | Term_appl : forall t u, Term t -> Term u -> Term (appl t u)
 | Term_abst : forall L t,
-  (forall x, ~ VSet.In x L -> Term (open t 0 (fvar x))) ->
+  (forall x, ~ VSet.In x L -> Term (t << fvar x)) ->
   Term (abst t)
 .
 
+Fixpoint subst (t : term) (x : Var.t) (r : term) :=
+match t with
+| fvar y => if Var.eq_dec x y then r else fvar y
+| bvar m => bvar m
+| appl t u => appl (subst t x r) (subst u x r)
+| abst t => abst (subst t x r)
+end.
 
+Notation "[ t | x := r ]" := (subst t x r).
 
+Inductive red : term -> term -> Prop :=
+| red_beta : forall t u, red (appl (abst t) u) (t << u)
+| red_appl_l : forall t u r, red t r -> red (appl t u) (appl r u)
+| red_appl_r : forall t u r, red u r -> red (appl t u) (appl t r).
 
 
 
