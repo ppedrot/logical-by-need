08c9a0616433c0e31eec22ede7af120133cd2fda
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-09-04 08:54:26 +0200

    more

--- vars.v
+++ vars.v
@@ -1,15 +1,34 @@
 Require MSets.
+Require Import Omega.
 
-Module Type FreshS
-  (Var : Orders.UsualOrderedType)
-  (VSet : MSetInterface.SetsOn(Var)).
+Module Type FreshListS (Var : Orders.UsualOrderedType).
+Parameter fresh : forall s : list Var.t, {v | ~ List.In v s}.
+End FreshListS.
 
+Module Type FreshS (Var : Orders.UsualOrderedType)(VSet : MSetInterface.SetsOn(Var)).
 Parameter fresh : forall s : VSet.t, {v | ~ VSet.In v s}.
-
 End FreshS.
 
-Module Var : Orders.UsualOrderedType := Coq.Arith.PeanoNat.Nat.
-Declare Module VSet : MSetInterface.SetsOn(Var).
+Module Type VarS := Orders.UsualOrderedType <+ FreshListS.
+
+Module Var : VarS.
+Include Coq.Arith.PeanoNat.Nat.
+
+Definition fresh : forall s : list Var.t, {v | ~ List.In v s}.
+Proof.
+intros l; pose (e := (List.fold_right Nat.max 0 l)).
+assert (H : List.Forall (fun x => x <= e) l).
+{ induction l; cbn in *; constructor; [apply Nat.le_max_l|].
+  eapply List.Forall_impl; [|eassumption].
+  cbn; intros; etransitivity; [eassumption|apply Nat.le_max_r]. }
+exists (S e); intros He.
+eapply List.Forall_forall in He; [|eassumption].
+cbn in He; omega.
+Qed.
+
+End Var.
+
+Module VSet : MSetInterface.SetsOn(Var) := MSetAVL.Make(Var).
 Declare Module Export Fresh : FreshS(Var)(VSet).
 
 Module Export VSetFacts := MSetFacts.WFactsOn(Var)(VSet).
