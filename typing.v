155cd58f6d3e67394314083c137fe5af589a2e87
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-09-04 08:24:18 +0200

    more

--- typing.v
+++ typing.v
@@ -9,9 +9,10 @@ Module Spec
 Module Import Lambda := lambda.Spec(Var)(VSet)(Fresh).
 
 Inductive type :=
-| arrow : type -> type
-| prod : type -> type
-| le : Var.t -> Var.t -> type.
+| arrow : type -> type -> type
+| prod : type -> type -> type
+| quant : type -> type
+| le : term -> term -> type.
 
 Definition env := list (Var.t * type).
 
