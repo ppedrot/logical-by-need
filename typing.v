7cf02522d66b2d9905bbdf94ef52851c5243579d
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-09-04 08:21:34 +0200

    more

--- typing.v
+++ typing.v
@@ -6,4 +6,15 @@ Module Spec
   (Import Fresh : lambda.Fresh(Var)(VSet))
 .
 
+Module Import Lambda := lambda.Spec(Var)(VSet)(Fresh).
+
+Inductive type :=
+| arrow : type -> type
+| prod : type -> type
+| le : Var.t -> Var.t -> type.
+
+Definition env := list (Var.t * type).
+
+Inductive typing : env -> term -> type -> Prop :=.
+
 End Spec.
