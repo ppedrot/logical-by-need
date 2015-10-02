c4ea4efaadaf3572265aeaf6cf361c6e29c92787
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-10-02 19:29:32 +0200

    fixing closure

--- closure.v
+++ closure.v
@@ -2,6 +2,5 @@ Require Import vars lambda.
 
 Inductive closure : term -> Prop :=
 | closure_bvar : closure (bvar 0)
-| closure_cons : forall x t C1 C2,
-  closure C1 -> closure C2 ->
-  closure ((C1 << (λ[x] (C2 << (fvar x)))) @ t).
+| closure_cons : forall C1 C2,
+  closure C1 -> closure C2 -> closure C1.
